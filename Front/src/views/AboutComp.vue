28f4ea3bc02c28fa0455d05c11507d9c861e7a5a
Stathis Kravaritis <efstathios.kravaritis@gmail.com>
2022-08-30 11:27:33 +0300

    Modal fix, 1st

--- Front/src/views/AboutComp.vue
+++ Front/src/views/AboutComp.vue
@@ -3,33 +3,15 @@
 <br/>
 <br/>
 <br/>
-<div class="home">
-    <ModalComp @close="toggleModal" :modalActive="modalActive">
-      <div class="modal-content">
-        <h1>This is a Modal Header</h1>
-        <p>This is a modal message</p>
-      </div>
-    </ModalComp>
-    <button @click="toggleModal" type="button">Open Modal</button>
-  </div>
+
 </template>
 
 <script>
-import ModalComp from '@/components/ModalComp.vue';
-import {ref} from 'vue'
+
 
 export default {
   name: "AboutComp",
-  components: { ModalComp },
-  setup() {
-    const modalActive = ref(false);
-
-    const toggleModal = () => {
-      modalActive.value = !modalActive.value;
-    };
-
-    return { modalActive, toggleModal };
-  }
+  
 }
 </script>
 
